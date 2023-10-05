26d8376967d56efb0c556e32bbd15b158a3a3744
Moises Abraham Vazquez Perez <mavazquezp@elektra.com.mx>
2023-10-04 21:39:09 -0600

    [ADD] Adding new CollectionView cell to test a integration

---
+++ SeriesYPeliculas/CollectionViewCells/CVExample/TestCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  TestCollectionViewCell.swift
+//  SeriesYPeliculas
+//
+//  Created by Moises Abraham Vazquez Perez on 04/10/23.
+//
+
+import UIKit
+
+class TestCollectionViewCell: UICollectionViewCell {
+    
+    //MARK: -  O U T L E T S
+    @IBOutlet weak var vwContainer: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
