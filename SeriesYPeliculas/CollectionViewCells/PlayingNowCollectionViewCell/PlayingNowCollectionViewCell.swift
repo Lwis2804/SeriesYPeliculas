0269535af52cb873a8cadacc689fb1035e3e5d53
LUIS <luis.enrique.enc@gmail.com>
2023-10-18 23:00:08 -0600

    [ADD] llamado a servicio de imágenes en playing now

--- SeriesYPeliculas/CollectionViewCells/PlayingNowCollectionViewCell/PlayingNowCollectionViewCell.swift
+++ SeriesYPeliculas/CollectionViewCells/PlayingNowCollectionViewCell/PlayingNowCollectionViewCell.swift
@@ -11,6 +11,7 @@ class PlayingNowCollectionViewCell: UICollectionViewCell {
     
     
   
+    @IBOutlet weak var imgPoster: UIImageView!
     @IBOutlet weak var npOriginalLanguage: UILabel!{
         didSet{self.npOriginalLanguage.layer.cornerRadius = 10}
     }
@@ -25,6 +26,14 @@ class PlayingNowCollectionViewCell: UICollectionViewCell {
         
     }
     
+    var downloadTask : URLSessionDownloadTask?
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        downloadTask?.cancel()
+        downloadTask = nil
+    }
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -34,9 +43,15 @@ class PlayingNowCollectionViewCell: UICollectionViewCell {
         self.npOriginalLanguage.text = categoria.original_language
         self.npOverView.text = categoria.overview
         self.npTitle.text = categoria.title
+        
+        if let urlPoster = categoria.poster_path,
+           let url = URL(string: "https://image.tmdb.org/t/p/w500\(urlPoster)"){
+            downloadTask = imgPoster.loadImage(url: url)
+        }
     }
     
     
-    
+    //"backdrop_path": "/cHNqobjzfLj88lpIYqkZpecwQEC.jpg"
+    //"poster_path": "/b0Ej6fnXAP8fK75hlyi2jKqdhHz.jpg"
 
 }
--- SeriesYPeliculas/Extensions/Extensions.swift
+++ SeriesYPeliculas/Extensions/Extensions.swift
@@ -33,3 +33,24 @@ extension UITableViewCell {
     class var identifier: String { return String(describing: self) }
     class var nib: UINib { return UINib(nibName: identifier, bundle: Bundle(for: SearchTableViewCell.self)) }
 }
+
+//MARK: - UI · I M A G E · V I E W · C E L L
+
+extension UIImageView {
+    /// Esta función permite la descarga de una imagen y colocarla en un UIImageView
+    /// - Parameter url: Recibe una URL de donde va a tomar el recurso
+    /// - Returns: una imagen descargada de internet
+    func loadImage(url: URL) -> URLSessionDownloadTask {
+        let session = URLSession.shared
+        let downloadTask = session.downloadTask(with: url){[weak self] url, response, error in
+            guard let self = self else { return }
+            if error == nil, let url = url, let data = try? Data(contentsOf: url), let image = UIImage(data: data){
+                DispatchQueue.main.async {
+                    self.image = image
+                }
+            }
+        }
+        downloadTask.resume()
+        return downloadTask
+    }
+}
